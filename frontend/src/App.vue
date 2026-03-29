73de37b4c70e91b9ddca2ae289ab4f733151f780
Носков Роман <romannskv02@gmail.com>
2026-03-29 08:34:35 +0700

    feat: Добавлены гриды

--- frontend/src/App.vue
+++ frontend/src/App.vue
@@ -1,7 +1,10 @@
 <template>
-  <div class="min-h-full min-w-full flex flex-col gap-10 items-center content-center justify-center mt-8">
+  <div class="min-h-full relative min-w-full flex flex-col gap-10 items-center content-center justify-center mt-8">
     <h1 class="font-bold text-2xl">Рисуй</h1>
-    <canvas ref="board" class="border border-gray-300"></canvas>
+    <div class="relative">
+      <canvas ref="board" class="border border-gray-300"></canvas>
+      <canvas ref="overlay" class="absolute inset-0 pointer-events-none"></canvas>
+    </div>
   </div>
 </template>
 
@@ -11,8 +14,9 @@ import {useCentrifugo} from "@/composables/useCentrifugo.ts";
 import {useCanvas} from "@/composables/useCanvas.ts";
 
 const board = ref<HTMLCanvasElement | null>(null);
+const overlay = ref<HTMLCanvasElement | null>(null);
 const {sub, centrifuge} = useCentrifugo()
-useCanvas(board, sub, centrifuge)
+useCanvas(board, overlay, sub, centrifuge)
 </script>
 
 <style scoped></style>
--- frontend/src/composables/useCanvas.ts
+++ frontend/src/composables/useCanvas.ts
@@ -1,24 +1,59 @@
 import {type Centrifuge, Subscription} from "centrifuge";
 import {onMounted, type Ref} from "vue";
 
-export function useCanvas(canvasRef: Ref<HTMLCanvasElement | null>, sub: Subscription, centrifuge: Centrifuge) {
+export function useCanvas(canvasRef: Ref<HTMLCanvasElement | null>, overlayRef: Ref<HTMLCanvasElement | null>, sub: Subscription, centrifuge: Centrifuge) {
   onMounted(() => {
     sub.subscribe()
     const canvas = canvasRef.value
     if (!canvas) {
       return
     }
+
+    const canvasOverlay = overlayRef.value
+    if (!canvasOverlay) {
+      return
+    }
+
     const ctx = canvas.getContext('2d')!
     if (!ctx) return
 
+    const overlayCtx = canvasOverlay.getContext('2d')!
+    if (!overlayCtx) return
+
     canvas.width = 1000
     canvas.height = 1000
+
+    canvasOverlay.width = 1000
+    canvasOverlay.height = 1000
+
+    drawGrid(overlayCtx)
 
     canvas.addEventListener('mousedown', (e) => {
       const rect = canvas.getBoundingClientRect()
       const x = Math.floor((e.clientX - rect.left) / 10)
       const y = Math.floor((e.clientY - rect.top) / 10)
       sub.publish({ type: 'pixel_paint', x, y, color:'#FF0000' })
+    })
+
+    let prevX = -1
+    let prevY = -1
+    canvas.addEventListener('mousemove', (e) => {
+      const rect = canvas.getBoundingClientRect()
+      const x = Math.floor((e.clientX - rect.left) / 10)
+      const y = Math.floor((e.clientY - rect.top) / 10)
+
+      if (x === prevX && y === prevY) return
+
+      if (prevX >= 0) {
+        overlayCtx.clearRect(0, 0, 1000, 1000) //todo Убийца оптимизации. Может сделать 3-ий канвас на highlight?
+        drawGrid(overlayCtx)
+      }
+
+      overlayCtx.fillStyle = 'rgba(136,120,120,0.4)'
+      overlayCtx.fillRect(x * 10, y * 10, 10, 10)
+
+      prevX = x
+      prevY = y
     })
 
     function drawPixel(x: number, y: number, color: string) {
@@ -46,4 +81,20 @@ export function useCanvas(canvasRef: Ref<HTMLCanvasElement | null>, sub: Subscri
       }
     })
   })
+
+  function drawGrid(overlayCtx: CanvasRenderingContext2D) {
+    overlayCtx.strokeStyle = 'rgba(0, 0, 0, 0.15)'
+    overlayCtx.lineWidth = 0.5
+
+    overlayCtx.beginPath()
+
+    for (let i = 0; i <= 1000; i += 10) {
+      overlayCtx.moveTo(i, 0)
+      overlayCtx.lineTo(i, 1000)
+      overlayCtx.moveTo(0, i)
+      overlayCtx.lineTo(1000, i)
+    }
+
+    overlayCtx.stroke()
+  }
 }
